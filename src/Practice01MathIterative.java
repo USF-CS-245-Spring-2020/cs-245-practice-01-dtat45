9fd08154ae33154af1fce376373185f9524d4124
dtat45 <dtat45@users.noreply.github.com>
2020-01-28 17:09:08 -0800

    finished assignment

---
+++ src/Practice01MathIterative.java
@@ -0,0 +1,28 @@
+
+public class Practice01MathIterative implements Practice01Math {
+	
+	public int fact(int n) throws Exception{
+		int f=1;
+		if(n<0)
+			throw new Exception("Answer is undefined...");
+		for(int i=1;i<=n;i++)
+			f*=i;
+		return f;
+	}
+	
+	public int fib(int n) throws Exception{
+		int fib=1;
+		int prevFib=1;
+		if(n<0)
+			throw new Exception("Answer is undefined...");
+		else if(n<=1)
+			return n;
+		for(int i=2;i<n;i++) {
+			int temp=fib;
+			fib+=prevFib;
+			prevFib=temp;
+		}
+		return fib;
+	}
+
+}
---
+++ src/Practice01MathRecursive.java
@@ -0,0 +1,19 @@
+
+public class Practice01MathRecursive implements Practice01Math{
+	
+	public int fact(int n) throws Exception {
+		if(n<0)
+			throw new Exception("Answer is undefined...");
+		else if(n==1)
+			return 1;
+		return n*fact(n-1);
+	}
+
+	public int fib(int n) throws Exception {
+		if(n<0)
+			throw new Exception("Answer is undefined...");
+		else if(n<=1)
+			return n;
+		return fib(n-1)+fib(n-2);
+	}
+}
